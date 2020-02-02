9d42c89404c361782de2b595dfd2f27ee5fd140f
Goldenfaeron <tobias@mreich.de>
2020-02-02 19:37:42 +0100

    mit if funktioniert aber noch nicht

--- fahren.py
+++ fahren.py
@@ -6,8 +6,9 @@ def cmfahren(geschwindigkeit,strecke):
    left_motor.reset_angle(0)
    right_motor.reset_angle(0)
    grad=360/(math.pi*wheel_diameter)*strecke*10
+   print(grad)
    left_motor.run_target(geschwindigkeit, grad, Stop.COAST, False)
-   right_motor.run_target(geschwindigkeit, grad, Stop.COAST, False)
+   right_motor.run_target(geschwindigkeit, grad)
 
    return;
 
@@ -18,10 +19,14 @@ def cmfahren2(geschwindigkeit,strecke):
    grad=360/(math.pi*wheel_diameter)*strecke*10
    left_motor.run_target(geschwindigkeit, grad, Stop.COAST, False)
    right_motor.run_target(geschwindigkeit, grad, Stop.COAST, False)
-   while left_motor.angle() <= grad:
-      print('works')
-      while left_motor.angle() > right_motor.angle():
-         print('bigger') 
-         wait(5000)
-         break
+   while abs(left_motor.angle()) <= grad:
+      print(abs(left_motor.angle()))
+      print(grad)
+      if left_motor.speed() > right_motor.speed():
+         left_motor.set_run_settings(right_motor.speed(), 0)
+         wait(500)
+      if left_motor.speed() < right_motor.speed():
+         right_motor.set_run_settings(left_motor.speed(), 0)
+         wait(500)
+   right_motor.run_target(geschwindigkeit, grad)
    return;
--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 from fahren import *
 
 # Write your program here
-print ("Test")
 brick.display.clear()
 
 #wait(1000)
-left_motor.set_run_settings(1000,100)
-right_motor.set_run_settings(1000,100)
-cmfahren2(100,500)
+#left_motor.set_run_settings(1000,100)
+#right_motor.set_run_settings(1000,100)
+cmfahren2(1000,100)
 wait(1000)
-cmfahren2(100,-500)
+cmfahren2(1000,-100)
 
